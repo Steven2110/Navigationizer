4c71660f08958b4f47406abd3a47986fce5c5f7c
Steven Wijaya <steven.wijaya2001@gmail.com>
2022-08-29 17:27:59 +0700

    Initial Commit

---
+++ Navigationizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Navigationizer
+//
+//  Created by Steven Wijaya on 29.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Navigationizer/NavigationizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationizerApp.swift
+//  Navigationizer
+//
+//  Created by Steven Wijaya on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
